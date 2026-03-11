882d4bfbc1fea7a26ddf8ef26de223c0adc85c3c
Francis <engukatu@yahoo.com>
2026-03-11 09:43:18 +0100

    logout fix

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,21 +1,39 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./PageNav.module.css";
 import Logo from "./Logo";
+import { useAuth } from "../contexts/FakeAuthContext";
+
 function PageNav() {
+  const { isAuthenticated, logout } = useAuth();
+
+  const navigate = useNavigate();
+
+  function handleClick() {
+    logout();
+    navigate("/");
+  }
+
   return (
     <nav className={styles.nav}>
       <Logo />
+
       <ul>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
+
         <li>
           <NavLink to="/product">Product</NavLink>
         </li>
+
         <li>
-          <NavLink to="/Login" className={styles.ctaLink}>
-            Login
-          </NavLink>
+          {isAuthenticated ? (
+            <button onClick={handleClick}>Logout</button>
+          ) : (
+            <NavLink to="/login" className={styles.ctaLink}>
+              Login
+            </NavLink>
+          )}
         </li>
       </ul>
     </nav>
